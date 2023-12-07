f8c33e80ad064c8d156a9c0159209eba2ebe63d1
maisylattanavong <maisylattanavong@gmail.com>
2023-12-07 13:41:17 +0700

    complete eposode22 Cart provider

---
+++ hooks/useCart.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState, useContext } from "react";
+type CartContextType = {
+    cartTotalQty:number;
+}
+
+export const CartContext = createContext<CartContextType | null>(null);
+
+interface Props{
+    [propName:string]:any;
+}
+
+export const CartContextProvider = ({props}: Props) => {
+    const [CartTotalQty, setCartTotalQty] = useState(0);
+    const value = {
+        CartTotalQty
+    }
+
+    return (<CartContext.Provider value={value} {...props} />);
+};
+
+
+export const useCart = ()=>{
+    const context = useContext(CartContext);
+    if(context === null){
+        throw new Error("useCart must be use within a CartContextProvider")
+    }
+    return context
+};
+
